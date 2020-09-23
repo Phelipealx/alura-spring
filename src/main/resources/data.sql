5b0d5b8a1a8caebac35c650a73163e2a0747b8e2
Phelipe <phelipemagalhaes13@gmail.com>
2020-09-23 11:01:35 -0300

    spring security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$MxzaLVeKwr8ThAaZ9iFBYOaESPhfp4yzxQolG8RZ8A6kfYsTMY2vW');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
